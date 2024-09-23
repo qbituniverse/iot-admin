8c5fec893e1edef964af6e25ce5c0d058b909bdc
Marcin Narloch <marcin.narloch@qbituniverse.com>
2024-09-23 14:06:36 +0200

    DB

--- IoT.Admin.Web/Program.cs
+++ IoT.Admin.Web/Program.cs
@@ -47,7 +47,7 @@ switch (webConfiguration.Repository!.Type)
 
     case "MongoDb":
         loggerConfiguration.WriteTo.MongoDBBson(
-            databaseUrl: $"{webConfiguration.Repository.MongoDb!.Url}/IoT-Admin",
+            databaseUrl: $"{webConfiguration.Repository.MongoDb!.Url}/IotAdmin",
             collectionName: "WebLogs",
             restrictedToMinimumLevel: Enum.Parse<LogEventLevel>(webConfiguration.LogLevel!.Database!));
         break;
